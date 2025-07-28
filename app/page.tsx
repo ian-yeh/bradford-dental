5923c54bbfd2979ebc6c241e305bb8b879779291
ian-yeh <user@users-MacBook-Air.local>
2025-07-28 16:27:50 -0400

    adding boilerplate for testimonials and choose us sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/components/homepage/HeroSection";
 import LocationSection from "@/components/homepage/LocationSection";
 import TeamSection from "@/components/homepage/TeamSection";
 import ServicesSection from "@/components/homepage/ServicesSection";
+import Testimonials from "@/components/homepage/Testimonials";
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
       <LocationSection />
       <TeamSection />
       <ServicesSection />
+      <Testimonials />
 
     </div>
   );
@@ -23,33 +25,3 @@ const Home = () => {
 
 export default Home;
 
-{/* Why Choose Us Section 
-      <div className="bg-white py-30 px-8">
-        <div className="max-w-[80%] mx-auto">
-          <div className="text-center mb-12">
-            <h2 className="text-4xl md:text-5xl font-bold font-josefin text-purple leading-snug">
-              Why Bradford Families Chose Us
-            </h2>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <ServiceCard 
-              title="General Dentistry"
-              description="Comprehensive care for all ages, from routine check-ups to complex procedures."
-              imageSrc="/images/general-dentistry.jpg"
-            />
-            <ServiceCard 
-              title="Cosmetic Dentistry"
-              description="Enhancing smiles with teeth whitening, veneers, and more."
-              imageSrc="/images/cosmetic-dentistry.jpg"
-            />
-            <ServiceCard 
-              title="Orthodontics"
-              description="Straightening teeth with braces and clear aligners for a healthier smile."
-              imageSrc="/images/orthodontics.jpg"
-            />
-
-          </div>
-        </div>
-      </div>
-
-      */}
---
+++ components/homepage/WhyChooseUs.tsx
@@ -0,0 +1,36 @@
+const WhyChooseUs = () => {
+  return (
+{/* Why Choose Us Section 
+      <div className="bg-white py-30 px-8">
+        <div className="max-w-[80%] mx-auto">
+          <div className="text-center mb-12">
+            <h2 className="text-4xl md:text-5xl font-bold font-josefin text-purple leading-snug">
+              Why Bradford Families Chose Us
+            </h2>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <ServiceCard 
+              title="General Dentistry"
+              description="Comprehensive care for all ages, from routine check-ups to complex procedures."
+              imageSrc="/images/general-dentistry.jpg"
+            />
+            <ServiceCard 
+              title="Cosmetic Dentistry"
+              description="Enhancing smiles with teeth whitening, veneers, and more."
+              imageSrc="/images/cosmetic-dentistry.jpg"
+            />
+            <ServiceCard 
+              title="Orthodontics"
+              description="Straightening teeth with braces and clear aligners for a healthier smile."
+              imageSrc="/images/orthodontics.jpg"
+            />
+
+          </div>
+        </div>
+      </div>
+
+      */}
+  )
+}
+
+export default WhyChooseUs
